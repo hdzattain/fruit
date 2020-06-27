02df21eba5af113466408bd848e5307846274f4e
hdzattain <58393005+hdzattain@users.noreply.github.com>
2020-06-27 16:48:08 +0800

    Update fruit.vue

--- fruit.vue
+++ fruit.vue
@@ -17,6 +17,18 @@
 	<button v-on:click="sum=pear*2+counter*3+dragon*5">算钱</button>
 	<p>您应付款 {{sum}} 元。</p>
 </div>
+<div id="app1">
+  <select v-model="selected" name="fruit">
+    <option value="">选择一个水果</option>
+    <option value="一斤3元">苹果</option>
+    <option value="一斤2元">梨子</option>
+	<option value="一个5元">火龙果</option>
+  </select>
+ 
+  <div id="output">
+      该水果单价是: {{selected}}
+  </div>
+</div>
 
 <script>
 new Vue({
@@ -26,6 +38,12 @@ new Vue({
 	pear:0,
 	dragon:0,
 	sum:0  
+  }
+})
+new Vue({
+  el: '#app1',
+  data: {
+	selected: '' 
   }
 })
 </script>
